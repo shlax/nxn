b0334e2d18aaa6e93c9689646e47b4ad6456da0b
pochodnicky <pochodnicky@gratex.com>
2024-06-07 13:34:50 +0200

    NxnCommand

--- src/main/scala/org/nxn/vulkan/NxnCommandPool.scala
+++ src/main/scala/org/nxn/vulkan/NxnCommandPool.scala
@@ -4,14 +4,14 @@ import org.lwjgl.system.MemoryStack
 import org.lwjgl.vulkan.{VK10, VkCommandPoolCreateInfo}
 import org.nxn.Extensions.*
 
-class NxnCommandPool(val device: NxnDevice) extends NxnContext, AutoCloseable{
+class NxnCommandPool(val device: NxnDevice, val queueFamilyIndex:Int) extends NxnContext, AutoCloseable{
   override val engine: NxnEngine = device.engine
 
   protected def init(): Long = MemoryStack.stackPush() | { stack =>
     val info = VkCommandPoolCreateInfo.calloc(stack)
       .sType$Default()
       .flags(0) //.flags(VK10.VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT)
-      .queueFamilyIndex(device.physicalDevice.preferredGraphicsQueueNodeIndex)
+      .queueFamilyIndex(queueFamilyIndex)
 
     val buff = stack.callocLong(1)
     vkCheck(VK10.vkCreateCommandPool(device.vkDevice, info, null, buff))
