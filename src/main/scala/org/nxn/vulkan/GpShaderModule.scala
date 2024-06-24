22f8395b798f6d152b0e3e50d62a832e2a5bb366
pochodnicky <pochodnicky@gratex.com>
2024-06-24 16:03:14 +0200

    GpShaderModule

---
+++ src/main/scala/org/nxn/vulkan/GpShaderModule.scala
@@ -0,0 +1,27 @@
+package org.nxn.vulkan
+
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.vulkan.{VK10, VkShaderModuleCreateInfo}
+import org.nxn.Extensions.*
+
+class GpShaderModule(val device: GpDevice, data:Array[Byte]) extends GpContext , AutoCloseable{
+  override val system: GpSystem = device.system
+
+  protected def init(dt:Array[Byte]):Long = MemoryStack.stackPush()|{ stack =>
+    val pCode = stack.malloc(dt.length).put(0, dt)
+
+    val info = VkShaderModuleCreateInfo.calloc(stack)
+      .sType$Default()
+      .pCode(pCode)
+
+    val lp = stack.callocLong(1)
+    vkCheck(VK10.vkCreateShaderModule(device.vkDevice, info, null, lp))
+    lp.get()
+  }
+
+  val shaderModule:Long = init(data)
+
+  override def close(): Unit = {
+    VK10.vkDestroyShaderModule(device.vkDevice, shaderModule, null)
+  }
+}
