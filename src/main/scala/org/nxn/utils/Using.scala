738aada2e0ccedf7389e92f3aeb0be6ed102f3d9
pochodnicky <pochodnicky@gratex.com>
2024-07-01 15:57:20 +0200

    inline

--- src/main/scala/org/nxn/utils/Using.scala
+++ src/main/scala/org/nxn/utils/Using.scala
@@ -6,7 +6,7 @@ object Using {
 
   extension [T <: AutoCloseable](ac: T) {
     @targetName("withClose")
-    inline def |[R](f: T => R): R = {
+    inline def | [R](f: T => R): R = {
       try {
         f.apply(ac)
       } finally {
@@ -23,11 +23,11 @@ object Using {
       t
     }
 
-    override def close(): Unit = {
+    inline override def close(): Unit = {
       for(c <- toClose) c.close()
     }
   }
-  
+
   inline def using[R](f: UsingManager => R): R = {
     val m = new UsingManager
     try {
