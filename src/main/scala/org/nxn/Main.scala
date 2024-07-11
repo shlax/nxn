4254aff39e84566d86dd4d632225724639b21c0e
pochodnicky <pochodnicky@gratex.com>
2024-07-11 15:14:07 +0200

    vkCmdPushConstants

--- src/main/scala/org/nxn/Main.scala
+++ src/main/scala/org/nxn/Main.scala
@@ -2,12 +2,12 @@ package org.nxn
 
 import org.lwjgl.system.{MemoryStack, MemoryUtil}
 import org.lwjgl.util.shaderc.Shaderc
-import org.lwjgl.vulkan.{VK10, VkCommandBuffer, VkDescriptorSetLayoutBinding, VkDescriptorSetLayoutCreateInfo, VkPipelineLayoutCreateInfo, VkPipelineVertexInputStateCreateInfo, VkPushConstantRange, VkVertexInputAttributeDescription, VkVertexInputBindingDescription}
+import org.lwjgl.vulkan.{VK10, VkCommandBuffer, VkPipelineLayoutCreateInfo, VkPipelineVertexInputStateCreateInfo, VkPushConstantRange, VkVertexInputAttributeDescription, VkVertexInputBindingDescription}
 import org.nxn.utils.Using.*
 import org.nxn.utils.{Dimension, FpsCounter}
 import org.nxn.vulkan.memory.MemoryBuffer
 import org.nxn.vulkan.shader.ShaderCompiler
-import org.nxn.vulkan.{Buffer, DescriptorPool, Fence, Pipeline, RenderCommand, Semaphore, TypeLength, VulkanSystem, vkCheck}
+import org.nxn.vulkan.{Buffer, Fence, Pipeline, RenderCommand, Semaphore, TypeLength, VulkanSystem}
 
 object Main extends Runnable{
 
@@ -59,21 +59,6 @@ object Main extends Runnable{
           b.put(0).put(1).put(2)
         })
 
-        /* layout(binding = 0) uniform UniformBufferObject{
-            mat4 viewMatrix;
-          } ubo;
-
-        val descPool = use(new DescriptorPool(sys.device, Map(VK10.VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER -> 1)))
-
-        val uniform = use(new Buffer(sys.device, 4 * 4 * TypeLength.floatLength.size, VK10.VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-          VK10.VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK10.VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)).map((memory: MemoryBuffer) => {
-          val b = MemoryUtil.memFloatBuffer(memory.address, memory.size)
-          b.put(1f).put(0f).put(0f).put(1f)
-          b.put(0f).put(1f).put(0f).put(1f)
-          b.put(0f).put(0f).put(1f).put(1f)
-          b.put(0f).put(0f).put(0f).put(1f)
-        }) */
-
         val triangle = use(new Pipeline(sys.renderPass, shaders){
 
           override protected def pipelineLayout(stack: MemoryStack, info: VkPipelineLayoutCreateInfo): Unit = {
@@ -83,24 +68,6 @@ object Main extends Runnable{
               .size(4 * 4 * TypeLength.floatLength.size)
 
             info.pPushConstantRanges(ranges)
-
-            /* pSetLayouts
-            val desc = VkDescriptorSetLayoutBinding.calloc(1, stack)
-            desc.get(0)
-              .descriptorType(VK10.VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER)
-              .stageFlags(VK10.VK_SHADER_STAGE_VERTEX_BIT)
-              .descriptorCount(1)
-              .binding(0)
-
-            val layoutInfo = VkDescriptorSetLayoutCreateInfo.calloc(stack)
-              .sType$Default()
-              .pBindings(desc)
-
-            val descriptorSetLayout = stack.callocLong(1)
-            vkCheck(VK10.vkCreateDescriptorSetLayout(renderPass.swapChain.device.vkDevice, layoutInfo, null, descriptorSetLayout))
-            descriptorSetLayout.flip()
-
-            info.pSetLayouts(descriptorSetLayout) */
           }
 
           // layout(location = 0) in vec2 inPosition
