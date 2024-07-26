469069fcbf4af47e91f4f4ed646608f22c4d38a3
pochodnicky <pochodnicky@gratex.com>
2024-07-26 08:50:35 +0200

    optimize

--- src/main/scala/org/nxn/model/ParsedModel.scala
+++ src/main/scala/org/nxn/model/ParsedModel.scala
@@ -12,8 +12,8 @@ class ParsedModel(val points:Array[Vector3f], val faces:Array[ParsedTriangle]){
   }
 
   def compile(epsilon:Float = 0.0000001f):IndexedModel = {
-    val vertexes = mutable.Map[Int, mutable.ArrayBuffer[(Int, VulkanVertex)]]()
-    val indexes = mutable.ArrayBuffer[VulkanTriangle]()
+    val vertexes = new Array[mutable.ArrayBuffer[(Int, VulkanVertex)]](points.length)
+    val indexes = new mutable.ArrayBuffer[VulkanTriangle](faces.length)
 
     def eqVector2f(a: Vector2f, b: Vector2f): Boolean = {
       Math.abs(a.x - b.x) < epsilon && Math.abs(a.y - b.y) < epsilon
@@ -30,7 +30,12 @@ class ParsedModel(val points:Array[Vector3f], val faces:Array[ParsedTriangle]){
 
     var next = 0
     def put(pv:ParsedVertex):Int = {
-      val v = vertexes.getOrElseUpdate(pv.index, new mutable.ArrayBuffer[(Int, VulkanVertex)])
+      var v = vertexes(pv.index)
+      if(v == null){
+        v = new mutable.ArrayBuffer[(Int, VulkanVertex)]
+        vertexes(pv.index) = v
+      }
+
       val f = v.find( x => eqVertex(x._2, pv) )
       if(f.isDefined) f.get._1 else {
         val act = next
@@ -43,10 +48,10 @@ class ParsedModel(val points:Array[Vector3f], val faces:Array[ParsedTriangle]){
     for(f <- faces) indexes.addOne(new VulkanTriangle(put(f.a), put(f.b), put(f.c)))
 
     val vertexArray = new Array[VulkanVertex](next)
-    for(i <- vertexes; j <- i._2) vertexArray(j._1) = j._2
+    for(i <- vertexes; j <- i) vertexArray(j._1) = j._2
 
-    val indMap = new Array[Array[Int]](vertexes.size)
-    for(i <- vertexes) indMap(i._1) = i._2.map(_._1).toArray
+    val indMap = new Array[Array[Int]](vertexes.length)
+    for(i <- vertexes.zipWithIndex) indMap(i._2) = i._1.map(_._1).toArray
 
     new IndexedModel(new VulkanModel(vertexArray, indexes.toArray), indMap)
   }
