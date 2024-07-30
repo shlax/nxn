95ec90fd51cbc1f74fdce02cfa4966ade570f287
peter o <slax.10101@gmail.com>
2024-07-30 19:53:11 +0200

    fix glfw cursor

--- src/main/scala/org/nxn/controls/MouseInput.scala
+++ src/main/scala/org/nxn/controls/MouseInput.scala
@@ -1,7 +1,7 @@
 package org.nxn.controls
 
 import org.lwjgl.glfw.{GLFW, GLFWCursorPosCallbackI, GLFWMouseButtonCallbackI, GLFWScrollCallbackI}
-import org.lwjgl.system.{MemoryStack, MemoryUtil}
+import org.lwjgl.system.MemoryUtil
 import org.nxn.math.Vector3f
 import org.nxn.utils.using.*
 import org.nxn.vulkan.GlfwWindow
@@ -9,8 +9,6 @@ import org.nxn.vulkan.GlfwWindow
 class MouseInput(val window: GlfwWindow) extends AutoCloseable{
   val cursor: Long = GLFW.glfwCreateStandardCursor(GLFW.GLFW_CROSSHAIR_CURSOR)
   GLFW.glfwSetCursor(window.glfwWindowHandle, cursor)
-
-  private var rotate = false
 
   private var xOff:Double = 0
   private var yOff:Double = 0
@@ -35,10 +33,17 @@ class MouseInput(val window: GlfwWindow) extends AutoCloseable{
     if(c != null) c.close()
   }
 
+  private var rotate = false
+  private var setXY = false
+
   closeCallback(GLFW.glfwSetCursorPosCallback(window.glfwWindowHandle, (win: Long, xpos: Double, ypos: Double) => {
     if (rotate) {
-      xOff += xpos - xWin
-      yOff += ypos - yWin
+      if (setXY) {
+        xOff += xpos - xWin
+        yOff += ypos - yWin
+      }else{
+        setXY = true
+      }
       xWin = xpos
       yWin = ypos
     }
@@ -47,18 +52,12 @@ class MouseInput(val window: GlfwWindow) extends AutoCloseable{
   closeCallback(GLFW.glfwSetMouseButtonCallback(window.glfwWindowHandle, (win: Long, button: Int, action: Int, mods: Int) => {
     if (button == GLFW.GLFW_MOUSE_BUTTON_2) {
       if (action == GLFW.GLFW_PRESS) {
-        MemoryStack.stackPush() | { stack =>
-          val x = stack.callocDouble(1)
-          val y = stack.callocDouble(1)
-          GLFW.glfwGetCursorPos(win, x, y)
-          xWin = x.get(0)
-          yWin = y.get(0)
-        }
         GLFW.glfwSetInputMode(win, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED)
         rotate = true
       }else if (action == GLFW.GLFW_RELEASE){
         GLFW.glfwSetInputMode(win, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL)
         rotate = false
+        setXY = false
       }
     }
   }))
