c48092292bc2252dd1de88ad81ba5cfc18f856fa
pochodnicky <pochodnicky@gratex.com>
2024-08-02 14:47:13 +0200

    AbstractJoint name lookup

--- src/main/scala/org/nxn/model/skeleton/AbstractJoint.scala
+++ src/main/scala/org/nxn/model/skeleton/AbstractJoint.scala
@@ -4,6 +4,15 @@ import org.nxn.math.Matrix4f
 
 abstract class AbstractJoint(val name:String, val vertexes:Array[SkinVertex], val subJoints:Array[AbstractJoint]){
 
+  def apply(nm:String): Option[AbstractJoint] = {
+    if(nm == name) Some(this)
+    else{
+      var res:Option[AbstractJoint] = None
+      for(a <- subJoints if res.isEmpty) res = a.apply(nm)
+      res
+    }
+  }
+
   def apply(modelMatrix:Matrix4f, normalMatrix:Matrix4f):Unit
 
   def update(modelMatrix:Matrix4f, normalMatrix:Matrix4f):Unit = {
