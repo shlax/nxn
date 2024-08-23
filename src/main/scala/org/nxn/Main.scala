08898d9f1545b50f107522945cad76a91d5c58e3
pochodnicky <pochodnicky@gratex.com>
2024-08-23 12:39:32 +0200

    sphere.m3d

--- src/main/scala/org/nxn/Main.scala
+++ src/main/scala/org/nxn/Main.scala
@@ -36,7 +36,7 @@ object Main extends Runnable{
       )
     }
 
-    val cube = (getClass.getResourceAsStream("/models/cube.m3d")| { in =>
+    val cube = (getClass.getResourceAsStream("/models/cube.m3d")| { in => // sphere
       ModelLoader().loadModel(in)
     }).invert(Axis.Y).compile().vulkanModel
 
