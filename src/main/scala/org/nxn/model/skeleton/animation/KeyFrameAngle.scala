a32f7c55bd101b3e6ec27c24a492dc14079479a2
pochodnicky <pochodnicky@gratex.com>
2025-08-11 16:59:41 +0200

    KeyFrameAngle

--- src/main/scala/org/nxn/model/skeleton/animation/KeyFrameAngle.scala
+++ src/main/scala/org/nxn/model/skeleton/animation/KeyFrameAngle.scala
@@ -2,6 +2,6 @@ package org.nxn.model.skeleton.animation
 
 import org.nxn.model.skeleton.AxisAngle
 
-class KeyFrameAngle(val angle: AxisAngle, value:Float) {
+class KeyFrameAngle(val angle: AxisAngle, val value:Float) {
 
 }
